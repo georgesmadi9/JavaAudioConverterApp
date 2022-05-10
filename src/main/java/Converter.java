c9b052fab956cf6f1aa4f77dcb395165a2306e06
georgesmadi9 <geo.mad1699@gmail.com>
2022-05-10 13:50:50 +0300

    Polished a few parts. Added more comments and documentation.

--- src/main/java/Converter.java
+++ src/main/java/Converter.java
@@ -8,13 +8,29 @@ import java.nio.file.attribute.FileTime;
 import java.util.Arrays;
 import java.util.HashMap;
 
+/****************************************************************************************************
+ * The Converter class is the one that will search through the source directory
+ * to retrieve the files that need to be converted and then has the option to convert file
+ * after file or all the files in the directory all at once.
+ *
+ * It is important ot note that there are some default values hardcoded into the class
+ * to make the app work correctly in case no parameter was given to it at the start
+ * Those values are the default directory within this project (resources/input and resources/output)
+ * they can be modified from the core (main) at the launch of the application if the user desires so
+ *
+ * Note: The array that is used to store the files names is limited to 100 entries it is not the limit
+ * of the application as is but is a default value that can be changed depending on the desired use
+ *******************************************************************************************************/
+
 public class Converter {
 
-    private File sourcePath = new File("./src/main/resources/input"); // source directory with default values
-    private File targetPath = new File("./src/main/resources/output"); // target directory with default values
-    private String[] filenames = new String[100]; // array with the names of the files to be converted
+    private final File sourcePath; // source directory
+    private final File targetPath; // target directory
+    private final String[] filenames = new String[100]; // array with the names of the files to be converted
 
     // Constructor to initialize the source and target directories passed as arguments
+    // and do the initial setup for the rest of the process
+    // Note: See comments on top of each function to see what they do
     public Converter(String sPath, String tPass) throws IOException {
         sourcePath = new File(sPath);
         targetPath = new File(tPass);
@@ -24,6 +40,8 @@ public class Converter {
 
     // Alternative constructor with default directories
     public Converter() throws IOException {
+        sourcePath = new File("./src/main/resources/input");
+        targetPath = new File("./src/main/resources/output");
         listFiles();
         sortByDateAsc();
     }
@@ -31,7 +49,7 @@ public class Converter {
     // Lists all the files in a directory (Debug Method Only)
     public void showFiles(File directory) {
         File[] files = directory.listFiles();
-        assert files != null;
+        assert files != null : "Directory is empty!";
         for (File file : files) {
             if (file.isDirectory()) {
                 System.out.println("Directory: " + file.getAbsolutePath());
@@ -58,18 +76,24 @@ public class Converter {
     // Note: Sorted names are stored in "filenames"
     public void sortByDateAsc() throws IOException {
         HashMap<Integer, String> dateName = new HashMap<>();
+
         for (String filename : filenames) {
+
             if (filename != null) {
                 Path file = Paths.get(sourcePath + "/" + filename);
                 BasicFileAttributes attr = Files.readAttributes(file, BasicFileAttributes.class);
-                FileTime time = attr.creationTime();
-                Integer newDate = (int) time.toMillis();
 
-                dateName.put(newDate, filename);
+                FileTime creationTime = attr.creationTime();
+                Integer creationDateMs = (int) creationTime.toMillis();
+
+                dateName.put(creationDateMs, filename);
             }
+
         }
+
         Object[] dates = dateName.keySet().toArray();
-        Arrays.sort(dates);
+        Arrays.sort(dates); // the basic sort was used here to avoid remaking sorting algorithm and make the process more complex
+
         for (int i = 0; i < dates.length; i++) {
             String file = dateName.get((int) dates[i]);
             filenames[i] = file.substring(0, file.length() - 4);
@@ -78,35 +102,46 @@ public class Converter {
 
     // Convert all the files in the source directory (sourcePath)
     public void convertAll() {
-        File[] sourcefiles = sourcePath.listFiles();
-        Logger logger = new Logger();
-        assert sourcefiles != null;
-        for (int i = 0; i < sourcefiles.length; i++) {
+        File[] sourceFiles = sourcePath.listFiles(); // array of file path (files that will be converted)
+        Logger logger = new Logger(); // Logger to save the successful processes into the csv files
+        assert sourceFiles != null : "No files found in source directory!"; // make sure that sourceFiles is not empty and give an error message if is empty
+
+        // Loop through all the files and convert them by using the "Engine"
+        // Saves each successful process into the csv file using the logger
+        for (int i = 0; i < sourceFiles.length; i++) {
+
             if (filenames[i] != null) {
                 System.out.println("Converting " + filenames[i] + " ...");
-                Engine e = new Engine(targetPath, sourcefiles[i], filenames[i]);
+
+                Engine e = new Engine(targetPath, sourceFiles[i], filenames[i]);
                 try {
-                    e.run();
+                    e.start();
                 } catch (Exception ex) {
-                    convertSingle(sourcefiles[i], filenames[i]);
+                    convertSingle(sourceFiles[i], filenames[i]);
                 }
+
                 System.out.println("Done \n");
+
                 logger.CSVLogger(e.getThreadUID(), filenames[i] + ".wav");
             }
         }
     }
 
     // Convert a single file in the source directory
+    // Saves the process in the CSV file it the conversion succeeded
     public void convertSingle(File sourceFile, String targetName) {
         System.out.println("Converting " + targetName + " ...");
+
         Logger logger = new Logger();
         Engine e = new Engine(targetPath, sourceFile, targetName);
         try {
-            e.run();
+            e.start();
         } catch (Exception ex) {
             convertSingle(sourceFile, targetName);
         }
+
         System.out.println("Done \n");
+
         logger.CSVLogger(e.getThreadUID(), targetName + ".wav");
     }
 
--- src/main/java/core.java
+++ src/main/java/core.java
@@ -1,14 +1,38 @@
-import javax.crypto.spec.PSource;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
-import java.util.Arrays;
 
 import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
+
+/************************************************************************
+ * This is main code of the application that needs to be executed.
+ * This file container the watcher that will monitor any change to
+ * directory (addition or modification of any file within the directory)
+ * and convert any new wav file added to a mp3 one.
+ *
+ * Note: the watcher uses the WatchService API to monitor the changes
+ * and is configured to any detect only changes that concern wav files.
+ *
+ *
+ * Note #2: This file can be run directly as is, or you can modify the
+ * input/output directories either within this main code or via command line
+ * by using this syntax:
+ *           java core <input directory> <output directory>
+ *
+ * Note #2.1: You need to compile the directory files first before
+ * running them. Use this to achieve that:
+ *           javac -d ./out/ ./src/main/java/*.java
+ * (Outputs all compile files to a new directory called out)
+ * You also need to make sure that when you run those commands you are
+ * in the correct directory.
+ ************************************************************************/
 
 public class core {
 
-    public static WatchService watcher;
+
+    public static WatchService watcher; // watcher creation
+    static Logger logs = new Logger(); // logger creation (will be used to change errors output)
 
     static {
         try {
@@ -18,16 +42,18 @@ public class core {
         }
     }
 
-    static Logger logs = new Logger();
-
+    // Monitor changes to the source directory
+    // Will only convert wav files and give errors if the file is not a .wav
+    // This method relies on the Watch Service API
     public static void watch(String directory, Converter converter) {
+        if (directory.equals("")) directory = "./src/main/resources/input"; // default directory if non is given
         Path dir = Paths.get(directory);
         try {
-            WatchKey key = dir.register(watcher, ENTRY_CREATE);
+            WatchKey key = dir.register(watcher, ENTRY_CREATE, ENTRY_MODIFY);
         } catch (IOException x) {
-            System.err.println(x);
+            x.printStackTrace();
         }
-        for (;;) {
+        for (; ;) {
 
             // wait for key to be signaled
             WatchKey key;
@@ -37,14 +63,14 @@ public class core {
                 return;
             }
 
-            for (WatchEvent<?> event: key.pollEvents()) {
+            for (WatchEvent<?> event : key.pollEvents()) {
                 WatchEvent.Kind<?> kind = event.kind();
 
                 // The filename is the context of the event.
-                WatchEvent<Path> ev = (WatchEvent<Path>)event;
+                WatchEvent<Path> ev = (WatchEvent<Path>) event;
                 Path filename = ev.context();
 
-                // Verify that the new file is a text file.
+                // Verify that the new file is a wav file.
                 try {
                     // Resolve the filename against the directory.
                     // If the filename is "test" and the directory is "foo",
@@ -55,11 +81,11 @@ public class core {
                     } else {
                         converter.convertSingle(
                                 new File(directory + "/" + filename),
-                                filename.toString().substring(0, filename.toString().length()-4)
+                                filename.toString().substring(0, filename.toString().length() - 4)
                         );
                     }
                 } catch (IOException x) {
-                    System.err.println(x);
+                    x.printStackTrace();
                 }
 
             }
@@ -74,17 +100,22 @@ public class core {
 
     // Main code to run the application
     public static void main(String[] args) throws IOException {
-        String sourcePath = "./src/main/resources/input";
-        String targetPath = "./src/main/resources/output";
-        if (args.length != 0) {
-            sourcePath = args[1];
-            targetPath = args[2];
-        }
-
         logs.errorLogger();
-        Converter cv = new Converter(sourcePath, targetPath);
-        cv.convertAll();
-        watch(sourcePath, cv);
+
+        String sourcePath = "";
+        String targetPath;
+
+        if (args.length != 0) {
+            sourcePath = args[0];
+            targetPath = args[1];
+            Converter cv = new Converter(sourcePath, targetPath);
+            cv.convertAll();
+            watch(sourcePath, cv);
+        } else {
+            Converter converter = new Converter();
+            converter.convertAll();
+            watch(sourcePath, converter);
+        }
     }
 
 }
